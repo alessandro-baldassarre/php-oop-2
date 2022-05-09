1de1e106dbcaf51ea6599606e46aa4f04a67509f
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-05-09 17:05:58 +0200

    created bases classes

--- classes/CreditCard.php
+++ classes/CreditCard.php
@@ -1,15 +1,14 @@
 <?php 
 
-include_once __DIR__ . "./User";
 
-class CreditCard extends User {
+class CreditCard{
 
      protected $bankName;
      protected $cardNumber;
      protected $expirationDate;
 
-    public function __construct (string $bankName, int $cardNumber, string $expirationDate, $firstName, $lastName, $email, $password){
-        parent::__construct($firstName, $lastName, $email, $password);
+    public function __construct (string $bankName, int $cardNumber, string $expirationDate){
+        
         $this->bankName = $bankName;
         $this->cardNumber = $cardNumber;
         $this->expirationDate = $expirationDate;
